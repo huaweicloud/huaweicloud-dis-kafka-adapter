e5298c301bda34b389d3169fa67ef8c5e8f55fe5
z30050220 <zongshengjie@huawei.com>
2023-11-14 18:16:42 +0800

    修改硬编码中有aksk

--- huaweicloud-dis-kafka-adapter-demo/src/main/java/com/huaweicloud/dis/demo/adapter/kafka/DISKafkaConsumerAssignDemo.java
+++ huaweicloud-dis-kafka-adapter-demo/src/main/java/com/huaweicloud/dis/demo/adapter/kafka/DISKafkaConsumerAssignDemo.java
@@ -15,32 +15,24 @@ import java.util.Properties;
 
 public class DISKafkaConsumerAssignDemo {
     private static final Logger LOGGER = LoggerFactory.getLogger(DISKafkaConsumerAssignDemo.class);
-
+    private static final String path = "dis.properties";
     public static void main(String[] args) {
-        // YOU AK
-        String ak = "YOU_AK";
-        // YOU SK
-        String sk = "YOU_SK";
-        // YOU ProjectId
-        String projectId = "YOU_PROJECT_ID";
-        // YOU DIS Stream
-        String streamName = "YOU_STREAM_NAME";
-        // 消费组ID，用于记录offset
-        String groupId = "YOU_GROUP_ID";
-        // DIS region
-        String region = "your region";
-
+        // 从配置文件中获取基础信息
         Properties props = new Properties();
-        props.setProperty(DISConfig.PROPERTY_AK, ak);
-        props.setProperty(DISConfig.PROPERTY_SK, sk);
-        props.setProperty(DISConfig.PROPERTY_PROJECT_ID, projectId);
-        props.setProperty(DISConfig.PROPERTY_REGION_ID, region);
+        try {
+            FileInputStream fileInputStream = new FileInputStream(path);
+            props.load(fileInputStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
         // LATEST: 从最新的数据之后开始消费；EARLIEST: 从最早的数据开始消费
         props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, OffsetResetStrategy.LATEST.name());
+        String streamName = props.getProperty("streamName");
 
         // 默认情况下不需要设置endpoint，会自动使用域名访问；如需使用指定的endpoint，解除如下注释并设置endpoint即可
         // props.setProperty(DISConfig.PROPERTY_ENDPOINT, "https://dis.cn-north-1.myhuaweicloud.com");
